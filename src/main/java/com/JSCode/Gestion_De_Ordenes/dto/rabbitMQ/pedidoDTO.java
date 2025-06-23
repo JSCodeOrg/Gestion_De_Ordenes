a6ce0adbaa3a3819b647ae5118d104de93125bb4
Juandavid611 <rojasnarvaezjuandavid0611@gmail.com>
2025-06-23 10:43:21 -0500

    Pruebas Unitarias de Services corregido

--- src/main/java/com/JSCode/Gestion_De_Ordenes/dto/rabbitMQ/pedidoDTO.java
+++ src/main/java/com/JSCode/Gestion_De_Ordenes/dto/rabbitMQ/pedidoDTO.java
@@ -1,3 +1,4 @@
+// File: Gestion_De_Ordenes/src/main/java/com/JSCode/Gestion_De_Ordenes/dto/rabbitMQ/PedidoDTO.java
 package com.JSCode.Gestion_De_Ordenes.dto.rabbitMQ;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,4 +8,5 @@ import lombok.Setter;
 public class PedidoDTO {
     private Long id;
     private String shippingAddress;
+
 }
